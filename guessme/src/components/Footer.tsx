01e0c29353d3c0115690b30ce997fee81e4f24e8
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-01-02 20:10:20 -0300

    fix index and add adjustments

--- guessme/src/components/Footer.tsx
+++ guessme/src/components/Footer.tsx
@@ -1,7 +1,14 @@
 export default function Footer() {
   return (
     <footer>
-      by <a href="https://github.com/Daniel-Macedo-dev" target="_blank" rel="noreferrer">Daniel Macedo</a>
+      by{" "}
+      <a
+        href="https://github.com/Daniel-Macedo-dev"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Daniel Macedo
+      </a>
     </footer>
   );
 }
